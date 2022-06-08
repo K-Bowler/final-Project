3a6e4d7b718812c242ec38d74019bce602f516b2
K-Bowler <Nebulasends@gmail.com>
2022-06-08 01:45:43 +0000

    issuse-1-users-can-view-memes WIP#3

--- client/components/nav-bar.jsx
+++ client/components/nav-bar.jsx
@@ -1,22 +1,64 @@
 import React from 'react';
 
-export default function NavBar(props) {
-  return (
+export default class NavBar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      menuOpen: false
+    };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    this.setState({ menuOpen: !this.state.menuOpen });
+  }
+
+  render() {
+    let menuClass = '';
+
+    if (!this.state.menuOpen) {
+      menuClass = 'phone-hidden';
+    }
+
+    return (
     <div>
-      <div className='row text-center'>
-        <div className='col-third'>
-          <h1 className='light-grey'>CREATE</h1>
+      <div className='row text-center justify-center align-center'>
+          <div id='create' className='col-full-col-third phone-hidden'>
+            <h1> <a onClick={this.handleClick} className='light-grey no-decoration' href="#create"> CREATE </a></h1>
+            <i onClick={this.handleClick} className="fas fa-chevron-down light-grey nav-arrow desktop-hidden"></i>
         </div>
-        <div className='col-third'>
-          <h1 className='light-grey'>MEMES</h1>
+          <div id='memes' className='col-full-col-third row justify-center align-center'>
+            <h1> <a onClick={this.handleClick} className='light-grey no-decoration' href="#memes"> MEMES </a></h1>
+            <i onClick={this.handleClick} className="fas fa-chevron-down light-grey nav-arrow desktop-hidden"></i>
         </div>
-        <div className='col-third'>
-          <h1 className='light-grey'>PROFILE</h1>
+          <div id='profile' className='col-full-col-third phone-hidden align-center'>
+            <h1> <a onClick={this.handleClick} className='light-grey no-decoration' href="#profile"> PROFILE </a></h1>
+            <i onClick={this.handleClick} className="fas fa-chevron-down light-grey nav-arrow desktop-hidden"></i>
         </div>
       </div>
       <div className='row justify-center'>
         <div className='nav-bar'></div>
       </div>
+      <div className={`hamberger-background desktop-hidden ${menuClass}`}>
+        <div className='hamberger'>
+          <div className='row'>
+            <div className='col-full'>
+                <h1><a className='dark-grey no-decoration' href="#create">CREATE</a></h1>
+            </div>
+          </div>
+          <div className='row'>
+            <div className='row col-full justify-center'>
+              <div className='menu-divider'></div>
+            </div>
+          </div>
+          <div className='row'>
+            <div className='col-full'>
+                <h1> <a className='dark-grey no-decoration' href="#profile">PROFILE</a></h1>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-  );
+    );
+  }
 }
